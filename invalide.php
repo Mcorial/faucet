84727154050fd79f7761e64b5f713946b1275990
Ecoriap <ecoriap@protonmail.com>
2021-12-03 14:06:12 +0100

    Create invalide.php

---
+++ invalide.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MyEcoria | Faucet</title>
+	<link rel="stylesheet" type="text/css" href="faucet.css">
+	<script src="https://js.hcaptcha.com/1/api.js" async defer></script>
+</head>
+<body>
+	<div class="valide">
+		</br>
+		<img src="images/invalide.png" width="100%" class="valideornot">
+		</br>
+	</div>
+</body>
+</html>
