8393131fdf5d11d2155750d5be1522fac71fe6cf
Ecoriap <ecoriap@protonmail.com>
2021-12-03 12:42:07 +0100

    Create valide.php

---
+++ valide.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MyEcoria | Faucet</title>
+	<link rel="stylesheet" type="text/css" href="css/faucet.css">
+	<script src="https://js.hcaptcha.com/1/api.js" async defer></script>
+</head>
+<body>
+	<div class="valide">
+		<img src="images/valide.png" width="100%">
+	</div>
+</body>
+</html>
