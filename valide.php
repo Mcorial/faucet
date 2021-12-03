ea8ada597cff8adde87d629c894cc044a48590f5
Ecoriap <ecoriap@protonmail.com>
2021-12-03 14:05:35 +0100

    Update valide.php

--- valide.php
+++ valide.php
@@ -2,12 +2,14 @@
 <html>
 <head>
 	<title>MyEcoria | Faucet</title>
-	<link rel="stylesheet" type="text/css" href="css/faucet.css">
+	<link rel="stylesheet" type="text/css" href="faucet.css">
 	<script src="https://js.hcaptcha.com/1/api.js" async defer></script>
 </head>
 <body>
 	<div class="valide">
-		<img src="images/valide.png" width="100%">
+		</br>
+		<img src="images/valide.png" width="100%" class="valideornot">
+		</br>
 	</div>
 </body>
 </html>
